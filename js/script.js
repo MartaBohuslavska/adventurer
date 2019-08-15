615c46538532a0c20ecb8e5c2d63a5717d3b8618
MartaSadova <sadovamarta3@ukr.net>
2019-08-16 00:26:52 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+  $('.item-collapse h2').on('click', function() {
+    $(this).parents('.item-collapse').siblings().removeClass('active');
+    $(this).parents('.item-collapse').addClass('active');
+  });
+
+  $(".myBar").each(function() {
+    $(this)
+      .data("origWidth", $(this).width())
+      .width(0)
+      .animate({
+        width: $(this).data("origWidth")
+      }, 1500);
+  });
+
+  $(function(){
+    $(".myBar > span").hide()
+    setTimeout(function() {
+      $(".myBar > span").show("slow")
+    }, 1000);
+  });
+
+});
