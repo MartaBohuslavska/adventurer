4692a84ab640fda8661f225c4f0ab73a0fa803fb
MartaSadova <sadovamarta3@ukr.net>
2019-09-09 23:59:02 +0300

    add scroll, works

--- js/script.js
+++ js/script.js
@@ -28,4 +28,42 @@ $(document).ready(function(){
     slidesToScroll: 1
   });
 
+  $("#all").click(function(){
+    $(".work").show();
+    return false
+  });
+
+  $("#photography").click(function(){
+    $(".work").hide();
+    $("#work-photography1").show();
+    $("#work-photography2").show();
+    return false
+  });
+
+  $("#graphic").click(function(){
+    $(".work").hide();
+    $("#work-graphic").show();
+    return false
+  });
+
+  $("#print").click(function(){
+    $(".work").hide();
+    $("#work-print1").show();
+    $("#work-print2").show();
+    return false
+  });
+
+  $("#web").click(function(){
+    $(".work").hide();
+    $("#work-web").show();
+    return false
+  });
+
+  $('a[data-target^="anchor"]').on('click.smoothscroll', function(){
+    var target = $(this).attr("href"),
+        bl_top = $(target).offset().top - 100;
+    $('body, html').animate({scrollTop: bl_top}, 700);
+  });
+  
+  870
 });
